ba62ae6a6c2df5b2f7a9075a67eeb863c541ee1b
markorn <markorn2002@gmail.com>
2016-06-16 18:36:10 -0500

    MOdificaciones

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 from miblog import views
 
 urlpatterns = [
+	url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list),
 ]
